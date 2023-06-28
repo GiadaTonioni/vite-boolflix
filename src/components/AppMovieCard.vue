f8ff6dda8675386de9f113131dbb6cc4a277708c
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-29 00:03:26 +0200

    bandiere lingue ridimensionate

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -10,12 +10,13 @@ export default {
         <img :src="`https://image.tmdb.org/t/p/w342/${myMovie.poster_path}`" alt="">
         <h3>{{myMovie.title || myMovie.name }}</h3>
         <h4>{{myMovie.original_title || myMovie.original_name}}</h4>
-        <h4>{{myMovie.original_language}}</h4>
+        <span>{{myMovie.original_language}}</span>
+        <img :src="`../../node_modules/flagpack/flags/1x1/${myMovie.original_language}.svg`" class="flag" alt="">
         <h4>{{myMovie.vote_average}}</h4>
     </div>
 </template>
 <style lang="scss" scoped>
-img {
-    width: 100%;
+.flag {
+    width: 25px;
 }
 </style>
